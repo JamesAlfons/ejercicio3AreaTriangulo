a4ca2d802d29a110603706978836559508608899
Santiago Developer <Santiago Developer@DESKTOP-S2D4PQE.fibertel.com.ar>
2023-05-26 11:12:14 -0300

    Ejercicio n#3 de Actividades afianciamiento. Escribe un programa que calcule el área de un triángulo a partir de la base y la altura ingresadas por el usuario.

---
+++ src/main/java/com/mycompany/practica3/Practica3.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Santiago Developer
+ */
+public class Practica3 {
+
+    public static void main(String[] args) {
+        
+        int base;
+        int altura;
+        String nombre;
+      
+        
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println(" Bienvenid@.Ingrese su nombre ");
+        nombre = leer.next();
+        
+         //Calcula el area con datos ingresados por usuario de base y altura.
+         System.out.println(" Bienvenid@ "+ nombre );
+         System.out.println(" Este es un programa que calcula el area de un triangulo segun los datos ingresados  ");
+         System.out.println("Ingrese datos de la base del triangulo : ");
+          base = leer.nextInt();
+          System.out.println("Ingrese datos de Altura del triangulo : ");
+          altura = leer.nextInt();
+         
+          double area = (base * altura ) / 2;
+          System.out.println("El area del Triangulo es : " + area);
+          
+         
+         
+         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
+        
+        
+    }
+}
